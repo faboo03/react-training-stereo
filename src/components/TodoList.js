fca79c3e2b4107bf76d3c1f77715fa8b4a7d8089
Fabien Rondeau <fabien.rondeau@stereograph.fr>
2021-11-23 15:39:13 +0100

    use completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Todo from './Todo'; 
 
-function TodoList({tasks}) {
+function TodoList(props) {
+    const {search} = useParams();
+
+    const [tasks, setTasks] = useState(props.tasks);
+
+    let filteredTasks = tasks;
+
+    if(search === "completed") {
+        filteredTasks = tasks.filter(task => task.completed);
+    }
+
 
     return (
         <>
             <h1 className="m-3">Liste de tâches</h1>
             <ul className="list-group m-3">
-                { tasks.map(
+                { filteredTasks.map(
                     (task,key) => <Todo todo={task} key={key} />
                 )}
             </ul>       
