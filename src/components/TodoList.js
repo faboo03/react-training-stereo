4292d33dd6967a1ef93c1ac23357c70749c19b63
Fabien Rondeau <fabien.rondeau@stereograph.fr>
2021-11-24 10:04:04 +0100

    fetch with server

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,11 @@
 import Todo from './Todo'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AddTodo from './AddTodo.js'
 import NavBar from './NavBar.js';
+import FetchJson from '../utils/FetchJson';
 
 export const statusEnum = {
-    LOADING:"loading",
+    LOADING: "loading",
     LIST: "list",
     ADD: "add",
     REMOVE: "remove"
@@ -13,9 +14,8 @@ export const statusEnum = {
 export const filterEnum = {
     ALL: "all",
     TODO: "todo",
-    COMPLETED:"completed"
+    COMPLETED: "completed"
 }
-
 
 function TodoList() {
     const [tasks, setTasks] = useState([]);
@@ -23,10 +23,28 @@ function TodoList() {
     const [listStatus, setListStatus] = useState(statusEnum.LIST);
     const [listFilter, setListFilter] = useState(filterEnum.ALL);
 
+    const fetchData = new FetchJson('http://localhost:3001');
+
+    useEffect(() => {
+        fetchData.getData( (data) => {
+            setTasks(data); 
+        }
+        )
+    }, []);
+
+
     //Handle Tasks states
     let addTask = (task) => {
+        fetchData.postData(task)
         setTasks([...tasks, task]);
         setListStatus(statusEnum.LIST);
+    }
+
+    let handleDelete = (currentTask) => {
+        fetchData.deleteData(currentTask);
+        let newTasks = [...tasks];
+        newTasks.splice(tasks.indexOf(currentTask), 1);
+        setTasks(newTasks);
     }
 
     let toggleCompleted = (currentTask) => {
@@ -40,12 +58,6 @@ function TodoList() {
         setTasks(currentTasks);
     }
 
-    let handleDelete = (currentTask) => {
-        let newTasks = [...tasks]; 
-        newTasks.splice(tasks.indexOf(currentTask),1);
-        setTasks(newTasks);
-    }
-    
     // handle filter tasks
     let filteredTasks = tasks;
     switch (listStatus) {
@@ -54,7 +66,7 @@ function TodoList() {
             break;
         case filterEnum.TODO:
             filteredTasks = tasks.filter(task => !task.completed);
-            break;    
+            break;
         default:
             filteredTasks = tasks;
             break;
@@ -64,18 +76,18 @@ function TodoList() {
         <section classID="todo">
             <h1 className="m-3">Liste de tâches</h1>
             <ul className="list-group m-3">
-                {statusEnum === filterEnum.LOADING ? 
-                        <li className="list-group-item d-flex align-items-center">Loading</li>
-                :
+                {statusEnum === filterEnum.LOADING ?
+                    <li className="list-group-item d-flex align-items-center">Loading</li>
+                    :
                     filteredTasks.length > 0 ?
                         filteredTasks.map(
                             (task, key) => <Todo task={task} toggleCompleted={toggleCompleted} handleDelete={handleDelete} listStatus={listStatus} key={key} />
                         ) :
-                        <li className="list-group-item d-flex align-items-center">Aucune Tâche</li>                
-            }
+                        <li className="list-group-item d-flex align-items-center">Aucune Tâche</li>
+                }
             </ul>
 
-            {listStatus === statusEnum.ADD? <AddTodo addTask={addTask} />:""}           
+            {listStatus === statusEnum.ADD ? <AddTodo addTask={addTask} /> : ""}
 
             <NavBar setListStatus={setListStatus} />
         </section>
@@ -83,3 +95,7 @@ function TodoList() {
 }
 
 export default TodoList;
+
+
+
+
---
+++ src/utils/FetchJson.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+
+export default class FetchJson {
+
+    constructor(url) {
+        axios.defaults.baseURL = url;
+    }
+    
+    getData = (callback = () => {}, callbackError = (err) => { console.log(err)}) => {
+        axios
+            .get('/todos')
+            .then((res) => {
+                callback(res.data);
+            })
+            .catch((err) => {
+                callbackError(err);
+            });
+    };
+
+
+    postData = (object, callback = () => {}, callbackError = (err) => { console.log(err)}) => {
+        axios
+            .post('/todos', object)
+            .then((res) => {
+                callback(res.data);
+            })
+            .catch((err) => {
+                callbackError(err);
+            });
+    }
+
+    deleteData = (object, callback = () => {}, callbackError = (err) => { console.log(err)}) => {
+        axios
+            .delete(`/todos/${object.id}`)
+            .then((res) => {
+                callback(res.data);
+            })
+            .catch((err) => {
+                callbackError(err);
+            });
+    }
+
+}
